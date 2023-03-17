c879d096ac67d17585e21b404811794d108d854a
nyaaku007 <127214791+nyaaku007@users.noreply.github.com>
2023-03-17 10:04:52 +0100

    Add files via upload
    week 2 task side hustle

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-
-if (valid) {
-    setTimeout(() => {
-        successMessage.style.display = "none";
-    },2000);
-    successMessage.classList.remove("hidden");
-    console.log("Name: ", Name);
-    console.log("Email: ", emailid);
-    console.log("Phone: ", phone);
-    console.log("Gender: ", genderMF);
-    console.log("Password: ", pass);
+function myFunction() {
+    var x= document.getElementById("myInput");
+    if (x.type === "password"){
+        x.type = "text";
+    } else{
+        x.type = "password" ;
+    }
 }
